ab192e39a2b888307dd9a6a5349369d66ee1574c
Edric Wong <ewonger@Edrics-Air.hsd1.ca.comcast.net>
2020-08-05 13:35:10 -0700

    changes

---
+++ Example.py
@@ -0,0 +1,19 @@
+import dash
+import dash_table
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+
+app.layout = dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict('records'),
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
